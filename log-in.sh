76fabc32274c77008f744261ee372eb6f499f684
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2022-06-13 00:28:35 +0300

    Add guard into log-in.sh to check whether pipenv is available

--- log-in.sh
+++ log-in.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+command -v pipenv >/dev/null 2>&1 ||
+{
+    echo 'pipenv is not found: $ pip3 install pipenv'
+    exit 1
+}
+
 cat ./Pipfile
 
 if [[ ${PIPENV_ACTIVE} != 1 ]]; then
